8b86484b1114a0add8e50e44e51faac833946e6b
Paul <pmwisdom@gmail.com>
2017-07-20 15:25:27 -0700

    Compile error

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :unity_gs, UnityGs.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "stormy-scrubland-45938.herokuapp.com", port: 443], force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
   config :unity_gs, UnityGs.Repo,
