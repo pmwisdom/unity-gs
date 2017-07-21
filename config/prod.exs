adcf519b9a2cad74efe09aa53375ad3ee9e6d1bd
Paul <pmwisdom@gmail.com>
2017-07-20 17:09:14 -0700

    Trying again

--- config/prod.exs
+++ config/prod.exs
@@ -15,13 +15,14 @@ config :unity_gs, UnityGs.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "stormy-scrubland-45938.herokuapp.com", port: 443], force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  check_origin: false
 
-  config :unity_gs, UnityGs.Repo,
-    adapter: Ecto.Adapters.Postgres,
-    url: System.get_env("DATABASE_URL"),
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    ssl: true
+config :unity_gs, UnityGs.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
